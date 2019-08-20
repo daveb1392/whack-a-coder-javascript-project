512ea69e34e0bd192e26673c905869ea9e527ea6
github username <github email address>
2019-08-20 17:43:08 +0100

    functinal front end

--- js/app.js
+++ js/app.js
@@ -1,26 +1,70 @@
 const holes = document.querySelectorAll(".hole");
-const moles = document.querySelectorAll(".mole");
+const coders = document.querySelectorAll(".mole");
 const btnStart = document.querySelector("button");
+btnStart.className = "btnStr"
 
 const startScreen = document.querySelector(".start-screen");
 const showScore = document.querySelector(".show-score");
-let currentScore = document.querySelector(".class")
+let currentScore = document.querySelector(".class");
 
 let score = 0;
 let lastHole;
 let timeUp = false;
-let gameTime = 5000;
 let increaseTime = 1;
 
 const state = {
-  gameTimer: 10
+  gameTimer: 9
+};
+
+
+const peep = () => {
+  const time = randomTime(400, 1000);
+  const hole = randomHole(holes);
+
+  hole.classList.add("up");
+
+  setTimeout(() => {
+    hole.classList.remove("up");
+    if (state.gameTimer) peep();
+  }, time);
+};
+
+const start = () => {
+  state.gameTimer = 9
+  timeUp = false;
+  peep();
+
+  timerStarting(() => {
+    timeUp = true;
+  }, state.gameTimer);
+};
+
+function startTimer(display) {
+  const gameTimer = setInterval(function() {
+    display.textContent = `${state.gameTimer}`;
+    state.gameTimer = --state.gameTimer;
+    if (state.gameTimer < 0) {
+      console.log("END!");
+      clearInterval(gameTimer);
+    }
+  }, 1000);
 }
+
+let timerStarting = () => {
+    display = document.querySelector("#time");
+  startTimer(display);
+};
+
+const bonk = e => {
+  state.gameTimer = state.gameTimer + increaseTime;
+  console.log("GUACAMOLE!!!!");
+};
 
 const randomTime = (min, max) => {
   return Math.round(Math.random() * (max - min) + min);
-}
+};
 
-const randomHole = (holes) => {
+const randomHole = holes => {
   const idx = Math.floor(Math.random() * holes.length);
   const hole = holes[idx];
 
@@ -29,81 +73,12 @@ const randomHole = (holes) => {
   }
   lastHole = hole;
   return hole;
-}
-
-const peep = () =>{
-  const time = randomTime(800, 1000);
-  const hole = randomHole(holes);
-
-  hole.classList.add("up");
-
-  setTimeout(() => {
-    hole.classList.remove("up");
-    if (!timeUp) peep();
-  }, time);
-}
-
-
-const start = () => {
-  score = 0;
-  // scoreBoard.textContent = score;
-  timeUp = false;
-  // scoreBoard.classList.remove("add");
-  // startScreen.classList.add("hide");
-
-  peep();
-
-  timerStarting(() => {
-    timeUp = true;
-    // startScreen.classList.remove("hide");
-
-    // if (score > 0) {
-    //   showScore.classList.add("show");
-
-    //   const message = "Your score: " + score + (score >= 10 ? " GREAT!" : "");
-    //   showScore.innerText = message;
-    // }
-
-  }, gameTime);
-}
-
-
-function startTimer(display) {
-  const gameTimer = setInterval(function () {
-    // debugger
-
-      // this is our starting timer reverse.
-      // seconds = seconds < 10 ? "0" + seconds : seconds;
-
-      display.textContent = `${state.gameTimer}`;
-      state.gameTimer = --state.gameTimer
-      if (state.gameTimer < 0) {
-          console.log('END!')
-          clearInterval(gameTimer)
-      }
-  }, 1000);
-}
-
-let timerStarting = () => {
-  let fiveSeconds = 2,
-      display = document.querySelector('#time');
-  startTimer(display);
 };
 
 
-const bonk = (e) => {
-  
 
-    // scoreBoard.classList.add("add");
-    state.gameTimer = state.gameTimer + increaseTime;
-    console.log("GUACAMOLE!!!!")
-    // scoreBoard.textContent = score;
-    // debugger
-  
-}
-
-moles.forEach(mole => {
-  mole.addEventListener("click", bonk);
+coders.forEach(coder => {
+  coder.addEventListener("click", bonk);
 });
 
 btnStart.addEventListener("click", start);
